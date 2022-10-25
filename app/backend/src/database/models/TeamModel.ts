24b585bb1893b9d68a78e9732d0ceea8b4b4aa1a
Julia <juliamarques61@gmail.com>
2022-10-25 15:06:24 -0300

    feat: model, migrations e seeders do Teams feito

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,25 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+export default class TeamModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
