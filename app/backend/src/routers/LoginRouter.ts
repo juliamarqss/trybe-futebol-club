28c1fa13726f994df7b85082fbf967c068d1efc1
Julia <juliamarques61@gmail.com>
2022-10-27 20:11:13 -0300

    fix: ajustando a validação do login

--- app/backend/src/routers/LoginRouter.ts
+++ app/backend/src/routers/LoginRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import LoginController from '../controllers/LoginController';
-// import loginValidation from '../utils/validations/loginValidation';
+import loginValidation from '../utils/validations/loginValidation';
 
 const loginRouter = Router();
 const loginController = new LoginController();
 
-loginRouter.post('/login', loginController.login);
+loginRouter.post('/login', loginValidation, loginController.login);
 // loginRouter.get()
 
 export default loginRouter;
--- app/backend/src/utils/validations/loginValidation.ts
+++ app/backend/src/utils/validations/loginValidation.ts
@@ -4,8 +4,8 @@ import { NextFunction, Request, Response } from 'express';
 const loginValidation = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json('All fields must be filled');
+  if (!email || !password || email === '' || password === '') {
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
   next();
 };
